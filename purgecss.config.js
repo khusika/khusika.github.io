7ebe210989f02b1f56a96ba509baad76edb3f874
Khusika Dhamar Gusti <mail@khusika.com>
2021-05-11 20:07:02 +0700

    CLI: purgecss: Safelist Vssue styles
    Signed-off-by: Khusika Dhamar Gusti <mail@khusika.com>

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,7 +2,7 @@ module.exports = {
     content: ['./docs/js/*.js', './docs/*.html', './docs/**/*.html', './docs/**/**/*.html',  './docs/**/**/**/*.html', './docs/**/**/**/**/*.html'],
     css: ['./docs/css/*.css'],
     safelist: {
-      deep: [/lazyloaded$/, /blur$/],
+      deep: [/lazyloaded$/, /blur$/, /vssue$/, /markdown-body$/],
       greedy: [/thumb$/, /lazyloading$/, /instagram-media$/, /lg-icon$/, /kbd$/]
     },
     blocklist: [ /search$/ ]
