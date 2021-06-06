06bdf6de0b4897d5fa96f883ec948db5f9c71482
Khusika Dhamar Gusti <mail@khusika.com>
2021-06-06 21:17:28 +0700

    CLI: Update purgecss config
    Signed-off-by: Khusika Dhamar Gusti <mail@khusika.com>

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,7 +3,7 @@ module.exports = {
     css: ['./docs/css/*.css'],
     safelist: {
       deep: [/lazyloaded$/, /blur$/, /vssue$/, /markdown-body$/],
-      greedy: [/thumb$/, /lazyloading$/, /instagram-media$/, /lg-icon$/, /kbd$/]
+      greedy: [/thumb$/, /lazyloading$/, /instagram-media$/, /lg-icon$/, /kbd$/, /fb_iframe_widget$/]
     },
     blocklist: [ /search$/ ]
 }
