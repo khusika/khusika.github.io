ee848bb571abe8169626bacf3fb2363e911acdf8
Khusika Dhamar Gusti <mail@khusika.com>
2021-09-13 20:25:30 +0700

    CLI: purgecss: Safelist smooth-scroll offset
    Signed-off-by: Khusika Dhamar Gusti <mail@khusika.com>

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,7 +2,7 @@ module.exports = {
     content: ['./docs/js/*.js', './docs/*.html', './docs/**/*.html', './docs/**/**/*.html',  './docs/**/**/**/*.html', './docs/**/**/**/**/*.html'],
     css: ['./docs/css/*.css'],
     safelist: {
-      deep: [/lazyloaded$/, /blur$/, /vssue$/, /markdown-body$/],
+      deep: [/lazyloaded$/, /blur$/, /vssue$/, /markdown-body$/, /target$/],
       greedy: [/thumb$/, /lazyloading$/, /instagram-media$/, /lg-icon$/, /kbd$/, /fb_iframe_widget$/]
     },
     blocklist: [ /search$/ ]
