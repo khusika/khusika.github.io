7672da06b842ccd3e5e350463c3ee50c6eceedb1
Khusika Dhamar Gusti <khusikadhamar@gmail.com>
2024-09-25 20:33:53 +0700

    CLI: Adjust buildjs and purgecss
    [deploy]
    
    Signed-off-by: Khusika Dhamar Gusti <khusikadhamar@gmail.com>

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,7 +2,7 @@ module.exports = {
     content: ['./docs/js/*.js', './docs/*.html', './docs/**/*.html', './docs/**/**/*.html',  './docs/**/**/**/*.html', './docs/**/**/**/**/*.html'],
     css: ['./docs/css/*.css'],
     safelist: {
-      deep: [/lazyloaded$/, /blur$/, /vssue$/, /markdown-body$/, /target$/, /path$/],
+      deep: [/lazyloaded$/, /blur$/, /markdown-body$/, /target$/, /path$/],
       greedy: [/thumb$/, /lazyloading$/, /instagram-media$/, /lg-icon$/, /kbd$/, /fb_iframe_widget$/]
     },
     blocklist: [ /search$/ ]
